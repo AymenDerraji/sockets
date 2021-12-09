2414ded773c9644701daf6bb55cf125044d5fe64
MrAymenDer <91115646+MrAymenDer@users.noreply.github.com>
2021-12-09 17:03:27 +0100

    created chat app
    i used socket and express to create this app

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require ("express");
+const http = require('http');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+const port = 3001;
+
+// när vi startar  o loggar in i localhost:3000 , då kommer vi till den sidan
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/socket.html");
+    
+});   
+
+// hanterar POST-förfrågningar
+// OBS - behövs för att läsa data som skickas med POST
+app.use(express.urlencoded({extended: true}));
+
+//här får vi data från guestBook.html, som skickas genom <form action="/" metodhe="post"> 
+// app.post("/", (req, res) => {
+    
+// });
+server.listen(port , () => console.log("listening on port: " + port));
+io.on('connection', (socket) => {
+        console.log('a user connected');
+        let tid = new Date().toISOString().substr(11, 8);   // aktuellt klockslag i formatet hh:mm:ss
+
+        socket.broadcast.emit("user-connected", `(${tid}) userId: ${socket.id}: Online!`);
+
+        socket.on('chat message', (msg) => {
+            console.log('message: ' + msg);
+            socket.broadcast.emit("chat message", `(${tid}) userId: ${socket.id}: ${msg}`);
+          });
+        socket.on('disconnect', () => {
+          console.log('user disconnected');
+        });
+  });
---
+++ public/client.js
@@ -0,0 +1,39 @@
+
+  var socket = io();
+
+  // if (input) {
+  // form.addEventListener("submit", function (e) {
+  //   e.preventDefault();
+  //     socket.emit("chat message", input.value);
+  //     input.value = "";
+  //   });
+  // }
+
+  window.onload = () => {
+    
+    // identifiera output-element
+    let output = document.getElementById("messages");
+
+    // ta emot användarinput och skicka meddelande
+    document.getElementById("form").addEventListener("submit", (evt) => {
+        evt.preventDefault();   // hindra att formuläret laddas om
+        let meddelande = document.getElementById("input").value;
+        socket.emit("chat message", meddelande); // skicka händelse till server
+        document.getElementById("input").value = "";   // rensa inmatningsfältet
+        let tid = new Date().toISOString().substr(11, 8);   // aktuellt klockslag i formatet hh:mm:ss
+        let html = `<li>Du skrev (${tid}): ${meddelande}</li>`;
+        output.innerHTML += html;
+    });
+
+    // ta emot meddelanden från servern och skriv ut
+    socket.on("chat message", (data) => {
+        let tid = new Date().toISOString().substr(11, 8);   // aktuellt klockslag i formatet hh:mm:ss
+        let html = `<li>${data}</li>`;
+        output.innerHTML += html;
+    })
+    socket.on("user-connected", (data) => {
+        let html = `<li>${data}</li>`;
+        output.innerHTML += html;
+    })
+
+}
